afc9f3b596d1f09f9ac4810494408419ae301c1e
Pratham Aggarwal <71117637+pratham15@users.noreply.github.com>
2020-10-24 15:40:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const weather = {};
+weather.temperature = {
+    unit : "celsius"
+}
+const KELVIN = 273;
+const key = "42546a4a56c1c15067fdfc6cd48fda97";
+
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
+
+
+let category = "top";
+
+function clicker(headingText) {
+    document.querySelectorAll("aside > a").forEach(function(el)
+    {
+        el.classList.remove("selected");
+    });
+
+    document.querySelector(`aside > a.${headingText.toLowerCase()}`).classList.add("selected")
+
+    let heading = document.querySelector('#newsList > h2');
+    heading.innerText = headingText + " Stories";
+
+    category = headingText.toLowerCase();
+    fetcher();
+}
+
+async function fetcher()
+{
+    document.querySelector("#topStoriesContainer").innerHTML = "";
+
+    const resp = await fetch(`https://hacker-news.firebaseio.com/v0/${category}stories.json`);
+    const ids = await resp.json();
+    //JSON = JavaScript Object Notation
+    //const itemsArray = [];
+
+    let items = ids.slice(0, 20);
+    console.log(items);
+
+    for(let i = 0 ; i < 20 ; i++)
+    {
+        const itemData = await fetch(`https://hacker-news.firebaseio.com/v0/item/${items[i]}.json`);
+        const resp = await itemData.json();
+        //itemsArray.push(resp);
+
+        const inserter = 
+        `<a class="news-item" href="${resp.url}" target="blank" data-title="${resp.title}">` +
+            `<h3 class="news-title">${resp.title}</h3>` +
+            `<p class="news-byline">${resp.by}</p>` +
+            `<p class="news-time">${resp.time}</p>` +
+            `<p class="news-score">` +
+                `<i class="fa fa-thumbs-up"></i>${resp.score}` +
+            `</p>` +
+        `</a>`;
+
+        console.log(inserter)
+
+        document.querySelector("#topStoriesContainer").insertAdjacentHTML("beforeend", inserter);
+    }
+}
+
+function main()
+{
+    console.log("Hey");
+}
+
+fetcher();
+main();
+
+function topClicked(){
+    clicker("Top");
+}
+
+function bestClicked(){
+    clicker("Best");
+}
+
+function newClicked(){
+    clicker("New");
+}
